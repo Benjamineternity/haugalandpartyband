8a8f7e9120e0238993f2f0bcc578cf4282499f42
Benjamin Hole <benjaminhole@Benjamins-MBP.lan>
2026-01-26 19:32:09 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container text-center mt-5 text-primary mt-2 mx-auto">
+    <h5>Haugaland Party Band 🎸</h5>
+  </div>
+</template>
+
+<style scoped></style>
