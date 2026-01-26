7c13966ce00192499ba3b7734c66b63d069b8cca
Benjamin Hole <benjaminhole@Benjamins-MBP.lan>
2026-01-26 21:18:41 +0100

    First test after setup before DNS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup lang="ts"></script>
-
 <template>
   <div class="container text-center mt-5 text-primary mt-2 mx-auto">
     <h5>Haugaland Party Band 🎸</h5>
   </div>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const areUser = ref(true)
+</script>
