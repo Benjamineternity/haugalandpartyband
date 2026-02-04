5609852c8fa9c20a0479d68778ff3cd698cd9db7
Benjamin Hole <benjaminhole@Benjamins-MacBook-Pro.local>
2026-02-04 22:26:26 +0100

    heh

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,9 @@
-// import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-
-// export default defineConfig(({ mode }) => ({
-//     base: mode === 'production' ? '/' : '/haugalandpartyband/',
-//     plugins: [vue()]
-// }));
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => ({
-  base: '/', //mode === 'production' ? '/' : '/haugalandpartyband/',
+  base: '/',
   plugins: [vue()],
   build: {
     rollupOptions: {
