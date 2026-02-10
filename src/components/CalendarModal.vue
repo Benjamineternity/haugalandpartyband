094e9a72ec0ed9ce4f47a353d5b51ba942317519
Benjamin Hole <benjaminhole@Benjamins-MacBook-Pro.local>
2026-02-10 23:15:17 +0100

    stuff

--- src/components/CalendarModal.vue
+++ src/components/CalendarModal.vue
@@ -1,14 +1,14 @@
 <template>
     <dialog v-if="showCalendarEvent"  ref="customDialog" open @keypress="keyPressFunc" class="customDialog"> <!--v-show="showCalendarEvent" draggable="true"-->
-        <div class="c-modal-header">
-            {{ props.events.length > 1 ? 'Arrangementer' : 'Arrangement' }}: {{ formatDate(props.day) }}
+        <div class="card-bg">
+            <h5 class="ms-2 mt-3">{{ props.events.length > 1 ? 'Arrangementer' : 'Arrangement' }}: {{ formatDate(props.day) }}</h5>
         </div>
-        <div class="events" v-for="event in props.events" :key="event.id">
-            <p class="mb-1"><i class="bi bi-bell me-1"></i>{{ event.name }}</p>
+        <div class="modal-body ms-3" v-for="event in props.events" :key="event.id">
+            <p class="mb-1 mt-2"><i class="bi bi-bell me-1"></i>{{ event.name }}</p>
             <p class="mb-1"><i class="bi bi-geo-alt me-1"></i>{{ event.location }}</p>
-            <p class="mb-1"><i class="bi bi-calendar-check me-1"></i>{{ formatDate(event.startDate) }} - {{ formatDate(event.endDate) }}</p> <!--:class="props.events.length > 1 ? 'more-events' : ''"-->
+            <p class="mb-1"><i class="bi bi-calendar-check me-1"></i>{{ formatDate(event.startDate) }} - {{ formatDate(event.endDate) }}</p>
         </div>
-        <div class="modal-footer">
+        <div class="modal-footer mt-2">
             <div class="mt-2 d-flex">
                 <button @click="showCalendarEvent = false" class="btn btn-sm btn-secondary c-default me-2">
                     Lukk
@@ -59,7 +59,6 @@
         if (!dsCalendar._value[e].link) {
             alert('Ingen link til dette arrangementet')
         } else {
-            console.log(dsCalendar._value[e].link)
             window.open(dsCalendar._value[e].link);
         }
     }
@@ -89,18 +88,21 @@
 <style scoped>
 
     .customDialog {
-        width: 45%;
-        min-width: 350px;
-        height: 40%;
-        min-height: 34%;
+        width: 500px;
+        min-width: fit-content;
+        height: 250px;
+        min-height: fit-content;
         top: 10%;
         left: 27.5%;
         bottom: 50%;
         z-index: 9999;
         display: flex;
         flex-direction: column;
-        border: 2px solid rgb(58, 58, 59);
+        border-bottom: 2px solid rgb(58, 58, 59);
         border-radius: 8px;
+        border-top: 0;
+        border-left: 0;
+        border-right: 0;
         background-color: rgb(58, 58, 59);
         color: rgb(242, 239, 234);
         padding: 0;
@@ -116,19 +118,26 @@
         background-color: rgb(58, 58, 59); /*rgba(22, 24, 31, var(--bs-bg-opacity)); */
     }
 
+    .card-bg {
+        border: 1px solid rgb(22, 24, 31);
+        border-radius: 8px;
+        --bs-bg-opacity: 0.5;
+        background-color: rgba(22, 24, 31, var(--bs-bg-opacity));
+    }
+
     .extra-border {
         border: 1px solid rgb(13,110,253);
         border-radius: 4px;
     }
 
-    .c-modal-header {
+    /* .c-modal-header {
         width: fit-content;
         height: 10%;
         border-bottom: 1px solid rgb(0, 0, 0);
         margin-top: 1%;
         margin-left: 2%;
         font-size: 1.2rem;
-    }
+    } */
 
     .modal-footer {
         height: 'fit-content';
