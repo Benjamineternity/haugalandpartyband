549c437253785b819aafdbb2bb8b0f0a3066053e
Benjamin Hole <benjaminhole@Benjamins-MacBook-Pro.local>
2026-02-18 21:34:07 +0100

    first http Request test

--- src/booking/Booking.vue
+++ src/booking/Booking.vue
@@ -5,17 +5,39 @@
         <Panel :text="'Booking'"> -->
             <p class="text-center">Book oss i dag!</p>
             <h3 class="text-center">BETA</h3>
-            <h6 class="text-center">For questions or features send a mail to Benjamin.hole@outlook.com please</h6>
+            <h6 class="text-center">For questions or features send a mail to Benjamin.hole@outlook.com please</h6><br>
+            <div class="container">
+                {{ httpReq }}
+            </div>
         <!-- </Panel>
     </div> -->
     <!-- <Footer /> -->
 </template>
 
 <script setup lang="ts">
+    import { ref, onMounted, onBeforeUnmount } from 'vue';
     // import NavBar from '/src/components/NavBar.vue';
     // import ImageBackground from '/src/components/ImageBackground.vue';
     // import Panel from '/src/components/Panel.vue';
     // import Footer from '/src/components/Footer.vue';
+
+    const httpReq = ref();
+
+    onMounted(async () => {
+        try {
+            const response = await fetch('https://hpb-hfh3f7dcckethfg8.westeurope-01.azurewebsites.net/api/HttpExample');
+            
+            if (!response.ok) { console.log("Error on get Request") };
+
+            console.log('response: ', response)
+
+            httpReq.value = response.text();
+
+        } catch (err) {
+            console.log('Error: ', err);
+        }
+
+    });
 
 </script>
 
